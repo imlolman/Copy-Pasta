11fd3843a2554857de0dbf47768ca70f0f06b35a
Satyam Gupta <satyam.gupta756@gmail.com>
2020-04-26 19:44:10 +0530

    Uploladed All Docs.

---
+++ assets/js/sketch.js
@@ -0,0 +1,43 @@
+myData = `What is Reinforcement Learning?
+Reinforcement learning (RL) is an area of machine learning concerned with how software agents ought to take actions in an environment in order to maximize the notion of cumulative reward. Reinforcement learning is one of three basic machine learning paradigms, alongside supervised learning and unsupervised learning.`
+let img, myFont;
+imgNum = 1
+fontNum = 1
+pageNum = 1
+xaxis=20
+yaxis=20
+fontsize=30
+w=700
+linespacing=false
+function preload() {
+    changeFont();
+    loadPage();
+}
+
+function setup(){
+    canvas = createCanvas(750,1000, WEBGL)
+    canvas.parent('contributing')
+    rectMode(CORNER);
+}
+
+function draw(){
+    translate(-width/2,-height/2,0);
+    image(img, 0, 0, width, height)
+    textFont(myFont);
+    textSize(fontsize)
+    fill('#264180')
+    if(linespacing){
+        textLeading(linespacing);
+    }
+    texture(img)
+    data = "\n"+myData
+    text(data, xaxis, yaxis, w, 900);
+}
+
+function changeFont(){
+    myFont = loadFont('fonts/font (2).ttf');
+}
+
+function loadPage(){
+    img = loadImage('pages/page (2).jpg');
+}
